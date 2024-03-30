6433caa01eda84df346769984fe4e8f4bff178b9
Dawn nguyen <dawnnguyen2021@gmail.com>
2024-03-30 01:45:19 -0400

    updated seed.js file

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,13 +1,24 @@
 const db = require('../config/connection');
-const { User } = require('../models');
+const { User, ManageSessions } = require('../models');
+
 const userSeeds = require('./userSeeds.json');
-const manageSession = require('../models')
-const manageSessions = require('./manageSession.json');
+const manageSessionsSeeds = require('./manageSessions.json');
 
 db.once('open', async () => {
   try {
     await User.deleteMany({});
     await User.create(userSeeds);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  process.exit(0);
+});
+
+db.once('open', async () => {
+  try {
+    await ManageSessions.deleteMany({});
+    await ManageSessions.create(manageSessionsSeeds);
   } catch (err) {
     console.error(err);
     process.exit(1);
