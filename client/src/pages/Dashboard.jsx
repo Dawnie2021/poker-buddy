dc3548f00c4c9e6869fcabf64290e3ef96c498e1
Dawn nguyen <dawnnguyen2021@gmail.com>
2024-03-30 00:19:26 -0400

    added tailwind css

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,8 @@
 export default function Dashboard() {
   return (
     <div>
-      <h1>DashBoard Page</h1>
+      <h1 className="text-3xl font-bold underline">
+        DashBoard Page</h1>
       <p>
         This is where all of the sessions will go!
       </p>
