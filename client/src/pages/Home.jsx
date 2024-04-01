3bc4bd49c99b4ea840f4d857991f12644b561b1e
Dawn nguyen <dawnnguyen2021@gmail.com>
2024-04-01 11:06:41 -0400

    added other images

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -27,3 +27,11 @@ function Home() {
 }
 
 export default Home;
+
+// https://tse3.mm.bing.net/th?id=OIP.VYVNwb8A4s4smdNvrEu-8wHaFM&pid=Api&P=0&h=220
+// https://tse2.mm.bing.net/th?id=OIP.n0UJok-JIPgmGkYdEdit-QHaEo&pid=Api&P=0&h=220
+// https://tse1.mm.bing.net/th?id=OIP.pBU4Pjw8sCImI5JqRK-pyQHaFL&pid=Api&P=0&h=220
+// https://tse2.mm.bing.net/th?id=OIP.RSYOhHxgeAQtFogIvQan4QHaE8&pid=Api&P=0&h=220
+// https://tse3.mm.bing.net/th?id=OIP.sLQ6TwBFj7gyVNg8FzLk6QHaEo&pid=Api&P=0&h=220
+// https://tse4.mm.bing.net/th?id=OIP.Bj-zHHqy_dEXc-KFeRcDAwHaEK&pid=Api&P=0&h=220
+// https://tse1.mm.bing.net/th?id=OIF.JNRCDy3fafWnkpasnOk1VA&pid=Api&P=0&h=220
