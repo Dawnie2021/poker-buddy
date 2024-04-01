bf154a6dedde897d384f5a98dce79a0ed1703e07
Dawn nguyen <dawnnguyen2021@gmail.com>
2024-04-01 11:13:35 -0400

    changed background image

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,7 +21,7 @@ function Home() {
     
   
     
-     <img src="https://img.freepik.com/free-vector/poker-game-background-with-playing-cards_1017-33703.jpg" width="100%"></img>
+     <img src="https://tse3.mm.bing.net/th?id=OIP.sLQ6TwBFj7gyVNg8FzLk6QHaEo&pid=Api&P=0&h=220" width="100%"></img>
   
   );
 }
@@ -30,8 +30,6 @@ export default Home;
 
 // https://tse3.mm.bing.net/th?id=OIP.VYVNwb8A4s4smdNvrEu-8wHaFM&pid=Api&P=0&h=220
 // https://tse2.mm.bing.net/th?id=OIP.n0UJok-JIPgmGkYdEdit-QHaEo&pid=Api&P=0&h=220
-// https://tse1.mm.bing.net/th?id=OIP.pBU4Pjw8sCImI5JqRK-pyQHaFL&pid=Api&P=0&h=220
 // https://tse2.mm.bing.net/th?id=OIP.RSYOhHxgeAQtFogIvQan4QHaE8&pid=Api&P=0&h=220
 // https://tse3.mm.bing.net/th?id=OIP.sLQ6TwBFj7gyVNg8FzLk6QHaEo&pid=Api&P=0&h=220
-// https://tse4.mm.bing.net/th?id=OIP.Bj-zHHqy_dEXc-KFeRcDAwHaEK&pid=Api&P=0&h=220
-// https://tse1.mm.bing.net/th?id=OIF.JNRCDy3fafWnkpasnOk1VA&pid=Api&P=0&h=220
+
