63a9b9921da218428d5d3edf3c1e2f0b9ff3c47d
Dawn nguyen <dawnnguyen2021@gmail.com>
2024-04-03 19:47:33 -0400

    made code more readable

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -29,11 +29,34 @@ const typeDefs = `#graphql
 
 
   type Mutation {
-    addUser(email:String!, username:String!, password:String!): Auth
-    login(email:String!, password:String!): Auth
-    addSession(startDate: String, endDate: String, location: String, results: Float,  hoursPlayed: Float,  notes: String): Session   
-    updateSession(id: ID, startDate: String, endDate: String, location: String, results: Float,  hoursPlayed: Float,  notes: String): Session
-    deleteSession(sessionId: ID!): Session  
+    addUser(
+    email:String!, 
+    username:String!, 
+    password:String!): Auth
+
+    login(
+    email:String!, 
+    password:String!): Auth
+
+    addSession(
+    startDate: String, 
+    endDate: String, 
+    location: String, 
+    results: Float,  
+    hoursPlayed: Float,  
+    notes: String): Session  
+
+    updateSession(
+    id: ID, 
+    startDate: String, 
+    endDate: String, 
+    location: String, 
+    results: Float,  
+    hoursPlayed: Float,  
+    notes: String): Session
+    
+    deleteSession(
+    sessionId: ID!): Session  
   }
 `;
 
