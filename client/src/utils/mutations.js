829944668eaddf5b14d88f695cf39d2d86d278ee
Dawn nguyen <dawnnguyen2021@gmail.com>
2024-04-04 18:48:11 -0400

    added my mutations and queries

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
@@ -20,6 +20,87 @@ export const ADD_USER = gql`
         _id
         username
       }
+    }
+  }
+`;
+
+export const ADD_SESSION = gql`
+  mutation addSession(
+    $sessionId: ID!
+    $startDate: Float!
+    $location: String!
+    $results: String!
+    $hoursPlayed: String!
+    $notes: String!
+  ) {
+    addSession(
+      sessionId: $sessionId
+      startDate: $startDate
+      location: $location
+      results: $results
+      hoursPlayed: $hoursPlayed
+      notes: $notes
+    ) {
+      _id
+      startDate
+      location
+      results
+      hoursPlayed
+      notes
+    }
+  }
+`;
+
+export const UPDATE_SESSION = gql`
+  mutation updateSession(
+    $sessionId: ID!
+    $startDate: Float!
+    $location: String!
+    $results: String!
+    $hoursPlayed: String!
+    $notes: String!
+  ) {
+    updateSession(
+      sessionId: $sessionId
+      startDate: $startDate
+      location: $location
+      results: $results
+      hoursPlayed: $hoursPlayed
+      notes: $notes
+    ) {
+      _id
+      startDate
+      location
+      results
+      hoursPlayed
+      notes
+    }
+  }
+`;
+
+export const DELETE_SESSION = gql`
+  mutation deleteSession(
+    $sessionId: ID!
+    $startDate: Float!
+    $location: String!
+    $results: String!
+    $hoursPlayed: String!
+    $notes: String!
+  ) {
+    deleteSession(
+      sessionId: $sessionId
+      startDate: $startDate
+      location: $location
+      results: $results
+      hoursPlayed: $hoursPlayed
+      notes: $notes
+    ) {
+      _id
+      startDate
+      location
+      results
+      hoursPlayed
+      notes
     }
   }
 `;
