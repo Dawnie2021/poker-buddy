48e7c82312118249d196e9430a3de26d50574fec
Dawn nguyen <dawnnguyen2021@gmail.com>
2024-04-05 23:23:21 -0400

    changed nav bar around

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -16,8 +16,10 @@ if (!Auth.loggedIn()) {
 if (Auth.loggedIn()) {
   navigation = navigation.concat([
     { name: "Dashboard", href: "./Dashboard", current: false },
-    { name: "Session", href: "./Session", current: false },
+    { name: "Add Session", href: "./Session", current: false },
+    { name: "Manage Sessions", href: "./ManageSession", current: false },
     { name: "Logout", onClick: Auth.logout, current: false },
+
   ]);
 }
 function classNames(...classes) {
@@ -89,19 +91,7 @@ export default function Navbar() {
                     leaveTo="transform opacity-0 scale-95"
                   >
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-2 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="./ManageSessions"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Manage Sessions
-                          </a>
-                        )}
-                      </Menu.Item>
+                     
                       <Menu.Item>
                         {({ active }) => (
                           <a
