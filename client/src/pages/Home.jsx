413287e45219d17142f232627b3e0f13ede5427c
Dawn nguyen <dawnnguyen2021@gmail.com>
2024-04-05 23:14:46 -0400

    deleted code from home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,21 +1,4 @@
-import { useQuery } from "@apollo/client";
-import { QUERY_USERS } from "../utils/queries";
-
-import UserList from "../components/UserList";
-
 function Home() {
-  const { data, loading, error } = useQuery(QUERY_USERS);
-
-  const users = data?.users || [];
-
-  if (error) {
-    throw Error(error);
-  }
-
-  if (loading) {
-    return <h2>Loading…</h2>;
-  }
-
   return (
     <img
       src="https://tse3.mm.bing.net/th?id=OIP.sLQ6TwBFj7gyVNg8FzLk6QHaEo&pid=Api&P=0&h=220"
@@ -25,8 +8,3 @@ function Home() {
 }
 
 export default Home;
-
-// https://tse3.mm.bing.net/th?id=OIP.VYVNwb8A4s4smdNvrEu-8wHaFM&pid=Api&P=0&h=220
-// https://tse2.mm.bing.net/th?id=OIP.n0UJok-JIPgmGkYdEdit-QHaEo&pid=Api&P=0&h=220
-// https://tse2.mm.bing.net/th?id=OIP.RSYOhHxgeAQtFogIvQan4QHaE8&pid=Api&P=0&h=220
-// https://tse3.mm.bing.net/th?id=OIP.sLQ6TwBFj7gyVNg8FzLk6QHaEo&pid=Api&P=0&h=220
