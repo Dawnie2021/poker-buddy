0322e61131e096514dc5e13adde05e99e65461f8
Dawn nguyen <dawnnguyen2021@gmail.com>
2024-04-07 20:23:56 -0400

    got the grapgh to work with data

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,36 +1,61 @@
 import React, { useEffect, useRef } from 'react';
 import { Chart, LineController, LinearScale, PointElement, CategoryScale, LineElement } from 'chart.js';
+import { useQuery } from "@apollo/client";
+import { QUERY_SESSIONS } from "../utils/queries";
 
 Chart.register(LineController, LinearScale, PointElement, CategoryScale, LineElement);
 
 const Dashboard = () => {
     const chartRef = useRef(null);
+    // function to get data form the query
+    const { data, loading, error } = useQuery(QUERY_SESSIONS);
+
+    // variable to store the data
+    const sessions = data?.sessions || [];
 
     useEffect(() => {
         if (chartRef && chartRef.current) {
-            const chartInstance = new Chart(chartRef.current, {
-                type: 'line',
-                data: {
-                    labels: ['Jan 3, 2024', 'Feb 9, 2024', 'Mar 15, 2024', 'Apr 1, 2024', 'Apr 4, 2024', 'Apr 5, 2024'],
-                    datasets: [{
-                        label: 'Profit',
-                        data: [100, 190, -300, 500, -200, 300],
-                        backgroundColor: 'rgba(94, 53, 62, 0.5)',
-                        borderColor: '#dd0f0f',
-                        borderWidth: 3,
-                       
-                    }]
-                },
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true
+            if (!loading && !error && sessions.length > 0) {
+
+                // sort the session by date
+                let sortedSessions = [...sessions].sort((a, b) => new Date(a.startDate) - new Date(b.startDate));
+
+                // format the date and results
+                let datesAndResults = sortedSessions.map((session) => {
+                  const date = new Date(session.startDate);
+                  const formattedDate = `${date.toLocaleString('default', { month: 'long' })} ${date.getDate()}, ${date.getFullYear()}`;
+                  return { date: formattedDate, results: session.results };
+                });
+                
+                // separate the dates and results to be used in graph
+                let dates = datesAndResults.map(item => item.date);
+                let results = datesAndResults.map(item => item.results);
+
+                // build the chart
+                const chartInstance = new Chart(chartRef.current, {
+                    type: 'line',
+                    data: {
+                        labels: dates,
+                        datasets: [{
+                            label: 'Profit',
+                            data: results,
+                            backgroundColor: 'rgba(94, 53, 62, 0.5)',
+                            borderColor: '#dd0f0f',
+                            borderWidth: 3,
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
                         }
                     }
-                }
-            });
-        }
-    }, []);
+                });
+            }
+        }}, 
+        // run the effect whenever the loading, error, or sessions change
+        [loading, error, sessions]);
 
     return (
         <div>
