30f5738cf8d31d3836bc0138b778c1729c7969be
Dawn nguyen <dawnnguyen2021@gmail.com>
2024-04-08 00:31:46 -0400

    got my log out button to work

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -105,7 +105,7 @@ export default function Navbar() {
                         {({ active }) => (
                           <a
                             href="./"
-                            onClick={(e) =>Auth.logout} 
+                            onClick={(e) =>Auth.logout()} 
                             className={classNames(
                               active ? "bg-gray-100" : "",
                               "block px-4 py-2 text-sm text-gray-700"
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -56,7 +56,7 @@ const Signup = () => {
             </div>
           </div>
           <div>
-            <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+            <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email Address</label>
             <div class="mt-2">
               <input id="email" name="email" type="email" value={formState.email} onChange={handleChange} autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
             </div>
