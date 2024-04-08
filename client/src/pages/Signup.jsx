01aa8b24429afcf515df8ba897f8428eb66fc110
Dawn nguyen <dawnnguyen2021@gmail.com>
2024-04-07 22:47:00 -0400

    changed the styling of signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -46,45 +46,47 @@ const Signup = () => {
       );
     }
     return (
-      <div class="flex justify-center p-20">
-      <form class="space-x-2" onSubmit={handleFormSubmit}>
-        <input class ="border-solid border-2 border-red-700 ..."
-          placeholder="Your username"
-          name="username"
-          type="text"
-          value={formState.name}
-          onChange={handleChange}
-        />
-        <input class ="border-solid border-2 border-red-700 ..."
-          placeholder="Your email"
-          name="email"
-          type="email"
-          value={formState.email}
-          onChange={handleChange}
-        />
-        <input class ="border-solid border-2 border-red-700 ..."
-          placeholder="******"
-          name="password"
-          type="password"
-          value={formState.password}
-          onChange={handleChange}
-        />
-        <button type="submit" class="bg-red-700 hover:bg-red-500 text-black-900 font-semibold hover:text-white py-1 px-2 border border-red-800 hover:border-transparent rounded">
-          SignUp
-        </button>
-      </form>
-      </div>
-    );
-  };
+      <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form class="space-y-6" onSubmit={handleFormSubmit}>
+        <div>
+            <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
+            <div class="mt-2">
+              <input id="username" name="username" type="text" value={formState.name} onChange={handleChange} autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            </div>
+          </div>
+          <div>
+            <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+            <div class="mt-2">
+              <input id="email" name="email" type="email" value={formState.email} onChange={handleChange} autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            </div>
+          </div>
 
-  return (
-    <main>
-      <div>
-        {renderForm()}
-        {error && <div>{error.message}</div>}
+          <div>
+            <div class="flex items-center justify-between">
+              <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+            </div>
+            <div class="mt-2">
+              <input id="password" name="password" type="password" value={formState.password} onChange={handleChange} autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            </div>
+          </div>
+
+          <div>
+            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+          </div>
+        </form>
       </div>
-    </main>
-  );
+    </div>
+);
 };
 
+return (
+<main>
+  <div>
+    {renderForm()}
+    {error && <div>{error.message}</div>}
+  </div>
+</main>
+);
+};
 export default Signup;
